d3683ade8b5ad2a8bf32a94a232a5645824e5cf2
Luiz Braz <luiz.braz@live.com>
2024-08-13 12:14:03 -0300

    Add prompt flow project

---
+++ NL2SQL-Chatbot/QueryDB.py
@@ -0,0 +1,34 @@
+from promptflow import tool
+from promptflow.connections import CustomConnection
+import pyodbc
+
+
+# The inputs section will change based on the arguments of the tool function, after you save the code
+# Adding type to arguments and return value will help the system show the types properly
+# Please update the function name/signature per need
+@tool
+def query_db(sql_query: str, SQL_conn: CustomConnection) -> str:
+    conn = pyodbc.connect(SQL_conn.conn)
+
+    # Create a cursor object to execute SQL queries
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute(sql_query)
+        
+        # Get the column names from cursor.description
+        columns = [column[0] for column in cursor.description]
+
+        # Initialize an empty list to store the results as dictionaries
+        results = []
+
+        # Fetch all rows and create dictionaries
+        for row in cursor.fetchall():
+            results.append(dict(zip(columns, row)))  
+    except Exception as e:
+        return f"Error: {e}"
+    cursor.close()
+    conn.close()
+
+    print(results)
+    return 'SQL Query: ' + str(results)
