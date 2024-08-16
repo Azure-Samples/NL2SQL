f21df685547d45d5f1ebbd1f8512c6f0147dd3a4
Pablo Castano <pablocastao@microsoft.com>
2024-08-16 18:26:09 -0300

    standarizing outputs

---
+++ core/tests.py
@@ -0,0 +1,7 @@
+sql_query = """```sql SELECT p.product_id, p.product_name, SUM(st.quantity * p.product_price) AS total_revenue
+FROM products p
+JOIN sales_transaction st ON p.product_id = st.product_id
+GROUP BY p.product_id, p.product_name
+ORDER BY total_revenue DESC
+LIMIT 1;```"""
+print(sql_query.strip("` \n").strip("sql "))
